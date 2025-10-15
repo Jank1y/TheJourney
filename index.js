04c775b93ccd8108f1a6969f9c97a82306f617a2
Jank1y <jan.t.k2005@gmail.com>
2025-10-15 12:30:01 +0200

    fixed the courosel

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ button.addEventListener('click', function() {
         counter.textContent = num;
     }
 });
+
+window.addEventListener('keydown', function(e){
+    if (e.key === "ArrowRight" || e.key === "d") {
+        counter.textContent = "Desno"
+    }else if (e.key === "ArrowUp" || e.key === "w") {
+        counter.textContent = "Gor"
+    }else if (e.key === "ArrowLeft" || e.key ==="a") {
+        counter.textContent = "Levo"
+    }else if (e.key === "ArrowDown" || e.key ==="s") {
+        counter.textContent = "Dol"
+    }
+})
